8366e623ddccde92284378f1041966ed66de8009
Rolando <echavarriandres113@gmail.com>
2024-08-08 10:59:14 -0500

    tu papa

---
+++ php/main.php
@@ -0,0 +1,28 @@
+<?php
+#tipos de variable
+$nombreVariable"hola mundo";
+$numero = 10 ;
+$decimal = 10.5;
+$bool = true;
+$array = [1, 2, 3];
+#estructura de control
+#si simple 
+if ("condicion"){
+
+}
+#si doble 
+if ("condicion"){
+echo "si";
+}else{
+    echo "no";
+}
+    #si anidado
+    if ("condicion"){
+        echo "si";
+    }else if ("condicion"){
+        echo "si";
+
+    }
+
+
+?>
